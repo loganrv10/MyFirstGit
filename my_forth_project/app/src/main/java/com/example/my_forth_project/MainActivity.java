7b5b42a6359a0ed64caa460e95082c3be834d827
SUBHAM <subhamsingh5338@gmail.com>
2021-06-26 14:45:45 +0530

    my 1Assignment

---
+++ my_forth_project/app/src/main/java/com/example/my_forth_project/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.my_forth_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private Button button2;
+    private Button button3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button =findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+             Toast.makeText(MainActivity.this, "Button 1 clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+        button2 =findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Button 2 clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        button3 =findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Button 3 clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
